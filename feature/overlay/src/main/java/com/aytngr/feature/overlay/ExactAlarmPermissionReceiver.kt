dce422cc1958d0c30b0f2d0f5c369ebde499a81c
aytn <a.garaisayeva@andersenlab.com>
2026-08-19 22:02:17 +0400

    added exact alarm permission change receiver

--- feature/overlay/src/main/java/com/aytngr/feature/overlay/ExactAlarmPermissionReceiver.kt
+++ feature/overlay/src/main/java/com/aytngr/feature/overlay/ExactAlarmPermissionReceiver.kt
@@ -1,5 +1,6 @@
 package com.aytngr.feature.overlay
 
+import android.app.AlarmManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -18,25 +19,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class BootReceiver: BroadcastReceiver() {
+class ExactAlarmPermissionReceiver: BroadcastReceiver() {
 
     @Inject lateinit var getNotesUseCase: GetNotesUseCase
     @Inject lateinit var reminderScheduler: ReminderScheduler
-    @Inject lateinit var getIsWidgetActiveUseCase: GetIsWidgetActiveUseCase
 
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
-            intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
-
+        if (intent.action == AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED) {
             val pending = goAsync()
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    if (getIsWidgetActiveUseCase() && Settings.canDrawOverlays(context)) {
-                        val serviceIntent = Intent(context, OverlayService::class.java)
-                        context.startForegroundService(serviceIntent)
-                    }
-                    val now = System.currentTimeMillis()
                     getNotesUseCase().first().onSuccess { notes ->
                         reminderScheduler.scheduleReminders(notes)
                     }
